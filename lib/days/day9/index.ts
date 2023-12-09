088ef80dfe6917e674eac0ce9bc602eed5216f9b
Richard Hubbard <richard.hubbard@armadry.co.uk>
2023-12-09 09:36:59 +0000

    Day 9

--- lib/days/day9/index.ts
+++ lib/days/day9/index.ts
@@ -1,19 +1,53 @@
 import { eg1, input } from './input';
-import { cleanAndParse } from '../../utils';
+import { cleanAndParse, sumOf } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function getStack(sequence: number[]) {
+  const stack: number[][] = [sequence];
+  let current = sequence;
+
+  while (!current.every(n => n === 0)) {
+    const next: number[] = [];
+
+    for (let i = 0, l = current.length - 1; i < l; i++) {
+      next.push(current[i + 1] - current[i]);
+    }
+
+    stack.push(next);
+    current = next;
+  }
+
+  return stack;
+}
+
+function findNext(sequence: number[]) {
+  const stack = getStack(sequence);
+  // sum of the last element
+  return sumOf(stack.map(s => s.at(-1)!));
+}
+
+function findPrevious(sequence: number[]) {
+  const stack = getStack(sequence);
+  // sum of the first element with alternating signs
+  return sumOf(stack.map((s, i) => (s[0] * (-1) ** i)));
+}
+
 
 export function part1() {
-  return "todo";
+  const sequences = cleanAndParse(input, l => l.split(" ").map(Number));
+
+  return sumOf(sequences.map(findNext));
 }
 
 export function part2() {
-  return "todo";
+  const sequences = cleanAndParse(input, l => l.split(" ").map(Number));
+
+  return sumOf(sequences.map(findPrevious));
 }
 
 export const answers = [
-  // 111,
-  // 222
+  1904165718,
+  964
 ];
