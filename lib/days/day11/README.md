defb2eee2f389eb1dada7cf75d03c764d48355c0
Richard Hubbard <richard.hubbard@armadry.co.uk>
2023-12-11 10:23:35 +0000

    Day 11

--- lib/days/day11/README.md
+++ lib/days/day11/README.md
@@ -1,3 +1,3 @@
 # Day 11
 
-
+I thought part 2 would be some sort of N dimensional wrapping of the space.
--- lib/days/day11/index.ts
+++ lib/days/day11/index.ts
@@ -1,19 +1,72 @@
 import { eg1, input } from './input';
-import { cleanAndParse } from '../../utils';
+import { cleanAndParse, generateArray, pairs } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function parse(input: string, age: number) {
+  const raw = cleanAndParse(input);
+
+  const width = raw[0].length;
+  const height = raw.length;
+
+  const srcList = raw.map((line, row) => {
+    return Object.entries(line.split(''))
+      .filter(([_, c]) => c === '#')
+      .map(([col, _]) => [row, Number(col)]);
+  }).flat();
+
+  const emptyRowSet = new Set(generateArray(height, i => i));
+  const emptyColSet = new Set(generateArray(width, i => i));
+
+  for (const [row, col] of srcList) {
+    emptyRowSet.delete(row);
+    emptyColSet.delete(col);
+  }
+
+  const emptyRows = Array.from(emptyRowSet).sort((a, b) => (a - b));
+  const emptyCols = Array.from(emptyColSet).sort((a, b) => (a - b));
+
+  const colMap = new Map(
+    generateArray(width, i => [i, i + (emptyCols.filter(c => c < i).length * (age - 1))])
+  );
+
+  const rowMap = new Map(
+    generateArray(height, i => [i, i + (emptyRows.filter(r => r < i).length * (age - 1))])
+  );
+
+  const list = srcList.map(
+    ([row, col]) => [rowMap.get(row), colMap.get(col)]
+  );
+
+  const size = srcList.length;
+
+  let total = 0;
+
+  for (const [a, b] of pairs(size)) {
+    const [rowA, colA] = list[a];
+    const [rowB, colB] = list[b];
+
+    total += Math.abs(rowA! - rowB!) + Math.abs(colA! - colB!);
+  }
+
+  return { srcList, list, size, width, height, emptyCols, colMap, emptyRows, rowMap, total };
+}
+
 
 export function part1() {
-  return "todo";
+  const { total } = parse(input, 2);
+
+  return total;
 }
 
 export function part2() {
-  return "todo";
+  const { total } = parse(input, 1_000_000);
+
+  return total;
 }
 
 export const answers = [
-  // 111,
-  // 222
+  9403026,
+  543018317006
 ];
