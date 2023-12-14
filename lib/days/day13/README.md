062090341917819f351138c0aa05766241489cf1
Richard Hubbard <richard.hubbard@armadry.co.uk>
2023-12-14 18:50:14 +0000

    Day 13 cruft removed

--- lib/days/day13/README.md
+++ lib/days/day13/README.md
@@ -1,1 +1,6 @@
 # Day 13
+
+In my opinion, part 2 of this puzzle was poorly worded. It implied that any old lines of reflection that were still valid should still be included in the answer.
+
+This is not the case. The only lines of reflection that should be included are the ones that are newly introduced by finding the smudge.
+
--- lib/days/day13/index.ts
+++ lib/days/day13/index.ts
@@ -11,8 +11,8 @@ type Map = {
   flippedMap: string[];
   hCandidates: number[];
   vCandidates: number[];
-  hAdjacents: number[][];
-  vAdjacents: number[][];
+  hAdjacents: number[];
+  vAdjacents: number[];
 };
 
 function blankMap(): Map {
@@ -27,13 +27,13 @@ function blankMap(): Map {
 }
 
 function parse(input: string) {
-  const lines = cleanAndParse(input);
+  const srcLines = cleanAndParse(input);
 
   const maps: Map[] = [];
 
   let currentMap = blankMap();
 
-  lines.forEach((line, i) => {
+  srcLines.forEach((line, i) => {
     if (line === '') {
       maps.push(currentMap);
       currentMap = blankMap();
@@ -45,31 +45,13 @@ function parse(input: string) {
 
   maps.push(currentMap);
 
-  return { lines, maps };
-}
-
-function flipMap(map: Map) {
-  const width = map.map[0].length;
-
-  for (let col = 0; col < width; col++) {
-    map.flippedMap[col] = map.map.map(row => row[col]).join('');
-  }
-}
-
-function unflipMap(map: Map) {
-  const width = map.flippedMap[0].length;
-
-  for (let col = 0; col < width; col++) {
-    map.map[col] = map.flippedMap.map(row => row[col]).join('');
-  }
-}
-
-function flipMaps(maps: Map[]) {
-  maps.forEach(flipMap);
-}
-
-function analyseMaps(maps: Map[]) {
   maps.forEach(map => {
+    const width = map.map[0].length;
+
+    for (let col = 0; col < width; col++) {
+      map.flippedMap[col] = map.map.map(row => row[col]).join('');
+    }
+
     map.hCandidates = [];
     map.vCandidates = [];
     map.hAdjacents = [];
@@ -89,22 +71,26 @@ function analyseMaps(maps: Map[]) {
 
     map.map.forEach((line, i) => {
       const distance = hammingDistance(line, map.map[i + 1]);
-      if (distance.length === 1) {
-        map.hAdjacents.push([i + 1, distance[0]]);
+      if (distance === 1) {
+        map.hAdjacents.push(i + 1);
       }
     });
 
     map.flippedMap.forEach((line, i) => {
       const distance = hammingDistance(line, map.flippedMap[i + 1]);
-      if (distance.length === 1) {
-        map.vAdjacents.push([i + 1, distance[0]]);
+      if (distance === 1) {
+        map.vAdjacents.push(i + 1);
       }
     });
   });
+
+  const lines = maps
+    .map((m, i) => findReflectionLines(m, i));
+
+  return { lines, maps };
 }
 
 function verifyReflectionLine(srcMap: Map, which: 'map' | 'flippedMap', line: number) {
-  // console.log({ which, line })
   const map = srcMap[which];
 
   const linesToTest = Math.min(
@@ -115,7 +101,7 @@ function verifyReflectionLine(srcMap: Map, which: 'map' | 'flippedMap', line: nu
   for (let i = 1; i < linesToTest; i++) {
     const a = line - i - 1;
     const b = line + i;
-    // console.log(map[a] === map[b], a, b, map[a], map[b])
+
     if (map[a] !== map[b]) {
       return false;
     }
@@ -140,11 +126,11 @@ function verifyNearReflectionLine(srcMap: Map, which: 'map' | 'flippedMap', line
 
     const distance = hammingDistance(map[a], map[b]);
 
-    if (distance.length >= 2) {
+    if (distance >= 2) {
       return [];
     }
-    if (distance.length === 1) {
-      nearMisses.push([a, b, distance[0]]);
+    if (distance === 1) {
+      nearMisses.push([a, b]);
     }
   }
 
@@ -165,35 +151,30 @@ function findReflectionLines(map: Map, i: number) {
   const hLines = map.hCandidates.filter(line => verifyReflectionLine(map, 'map', line));
   const vLines = map.vCandidates.filter(line => verifyReflectionLine(map, 'flippedMap', line));
 
-  const hAdjs = map.hAdjacents.filter(line => verifyReflectionLine(map, 'map', line[0]));
-  const vAdjs = map.vAdjacents.filter(line => verifyReflectionLine(map, 'flippedMap', line[0]));
+  const hAdjs = map.hAdjacents.filter(line => verifyReflectionLine(map, 'map', line));
+  const vAdjs = map.vAdjacents.filter(line => verifyReflectionLine(map, 'flippedMap', line));
 
   return { hLines, vLines, hNears, vNears, hAdjs, vAdjs };
 }
 
 function hammingDistance(a: string, b: string) {
-  let differences: number[] = [];
+  let difference = 0;
 
   if (!b) {
-    return Array.from(a, (_, i) => i);
+    return a.length;
   }
 
   for (let i = 0, l = a.length; i < l; i++) {
     if (a[i] !== b[i]) {
-      differences.push(i);
+      difference++;
     }
   }
 
-  return differences;
+  return difference;
 }
 
 export function part1() {
-  const { maps } = parse(input);
-  flipMaps(maps);
-  analyseMaps(maps);
-
-  const lines = maps
-    .map((m, i) => findReflectionLines(m, i))
+  const { lines } = parse(input);
 
   const sum = lines
     .map(({ hLines, vLines }, i) => {
@@ -207,108 +188,20 @@ export function part1() {
 }
 
 export function part2() {
-  const { maps } = parse(input);
-  flipMaps(maps);
-  analyseMaps(maps);
-
-  const linesV1 = maps
-    .map((m, i) => findReflectionLines(m, i));
-
-  maps.forEach((map, i) => {
-    const line = linesV1[i];
-
-    for (const [ref, [a, b, col]] of line.hNears) {
-      if (line.vLines.includes(col)) {
-        throw new Error(`Overlap hNears with vLines at ${i}`);
-      }
-    }
-
-    for (const [ref, [a, b, row]] of line.vNears) {
-      if (line.hLines.includes(row)) {
-        throw new Error(`Overlap vNears with hLines at ${i}`);
-      }
-    }
-
-    if ((line.hAdjs.length + line.vAdjs.length + line.hNears.length + line.vNears.length) !== 1) {
-      throw new Error(`Not exactly one candidate at ${i}`);
-    }
-
-    if (line.hAdjs.length || line.hNears.length) {
-      if (line.hAdjs.length) {
-        const [[a]] = line.hAdjs;
-        const b = a - 1
-        map.map[a] = map.map[b];
-      }
-      else {
-        const [[, [a, b]]] = line.hNears;
-        map.map[a] = map.map[b];
-      }
-
-      flipMap(map);
-    }
-    else {
-      if (line.vAdjs.length) {
-        const [[a]] = line.vAdjs;
-        const b = a - 1
-        map.flippedMap[a] = map.flippedMap[b];
-      }
-      else {
-        const [[, [a, b]]] = line.vNears;
-        map.flippedMap[a] = map.flippedMap[b];
-      }
-
-      unflipMap(map);
-    }
-  });
-
-  analyseMaps(maps);
-
-  const lines = maps
-    .map((m, i) => findReflectionLines(m, i));
-
-  // lines.forEach((line, i) => {
-  //   const lineV1 = linesV1[i];
-
-  //   if (line.hLines[0] === lineV1.hLines[0]) {
-  //     console.log(`(Pt 1) Both hSum and vSum at ${i}`);
-  //   }
-  // });
+  const { lines } = parse(input);
 
   const sum = lines
-    .map(({ hLines, vLines }, i) => {
-      const hSum = hLines[0] || 0;
-      const vSum = vLines[0] || 0;
-
-      // if (hSum > 0 && vSum > 0) {
-      //   console.log(`(Pt 2) Both hSum and vSum at ${i}`);
-      // }
-
-      return (100 * hSum) + vSum;
-    });
-
-  const sumV1 = linesV1
     .map(({ hAdjs, vAdjs, hNears, vNears }, i) => {
-      const hSum = hAdjs[0]?.[0] || hNears[0]?.[0] || 0;
-      const vSum = vAdjs[0]?.[0] || vNears[0]?.[0] || 0;
-
-      if (hSum > 0 && vSum > 0) {
-        console.log(`(Pt 2) Both hSum and vSum at ${i}`);
-      }
-
-      // console.log({ hSum, vSum, i });
+      const hSum = hAdjs[0] || hNears[0]?.[0] || 0;
+      const vSum = vAdjs[0] || vNears[0]?.[0] || 0;
 
       return (100 * hSum) + vSum;
     });
 
-  console.log({ linesV1, lines, maps, sum, sumV1 });
-
-  // 43000 is too high
-  // 43372 is too high
-  // 52815 is too high
-  return sumOf(sumV1);
+  return sumOf(sum);
 }
 
 export const answers = [
   37718,
-  400 // for eg1
+  40995
 ];
